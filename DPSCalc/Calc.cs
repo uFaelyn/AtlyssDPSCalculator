6bb0a59918425aed0493ac49f3f8826f4dd82dee
SevenDeadlyWinds <zachmasterb7467@gmail.com>
2024-12-04 09:31:29 -0600

    Fixed an issue with Decay

--- DPSCalc/Calc.cs
+++ DPSCalc/Calc.cs
@@ -2,6 +2,8 @@
 using HarmonyLib;
 using System.Timers;
 using System.Linq.Expressions;
+using JetBrains.Annotations;
+using UnityEngine;
 
 [assembly: MelonInfo(typeof(DPSCalc.Calc), "DPSCalc", "1.1.0", "Faelynox", null)]
 [assembly: MelonGame("KisSoft", "ATLYSS")]
@@ -12,9 +14,9 @@ namespace DPSCalc
     {
         public static Calc instance;
         public Player localPlayer;
-        public static List <int> damageList = [];
-        public static List <int> dpsList = [];
-        public static List <int> dpsList2 = [];
+        public static List<int> damageList = [];
+        public static List<int> dpsList = [];
+        public static List<int> dpsList2 = [];
         public static int finalDPS;
         private System.Timers.Timer timer;
         public override void OnInitializeMelon()
@@ -34,31 +36,53 @@ namespace DPSCalc
                 timer.Start();
                 MelonLogger.Msg("[DEBUG] Timer started!");
             }
+        }
 
+        [HarmonyPatch(typeof(Player), "OnGameConditionChange")]
+        public class playerSpawn
+        {
+            public static void Postfix(ref Player __instance)
+            {
+                if (__instance._currentGameCondition == GameCondition.IN_GAME && __instance.isLocalPlayer)
+                {
+                    Calc.instance.localPlayer = __instance;
+                    MelonLogger.Msg($"[DEBUG] localPlayer:{Calc.instance.localPlayer}");
+                }
+            }
+        }
+        public override void OnUpdate()
+        {
+            if (localPlayer == null) { return; }
 
+            if (localPlayer._inChat || localPlayer._inUI || HostConsole._current._isOpen || localPlayer._bufferingStatus) { return; }
+
+            else if (Input.GetKeyDown(KeyCode.U))
+            {
+                dpsList.Clear();
+                dpsList2.Clear();
+                MelonLogger.Msg("[INPUT] Lists cleared!");
+            }
         }
         public void onTimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
+            int maxValue = 0;
+            void removeMaximum()
+            {
+                if (maxValue != 0)
+                {
+                    dpsList2.Remove(maxValue);
+                }
+                else if (dpsList2.Count() > 0)
+                {
+                    dpsList2.RemoveAt(0);
+                }
+            }
             if (dpsList.Count > 0)
             {
                 try
                 {
-                    int maxValue = 0;
+
                     double averageDPSoverTime;
-
-                    void clearLists()
-                    {
-                        dpsList2.Clear();
-                    }
-                    void removeMaximum()
-                    {
-                        if (maxValue != 0)
-                        {
-                            dpsList2.Remove(maxValue);
-                        }
-                    }
-
-
                     int avgdps = dpsList.Sum();
                     dpsList2.Add(avgdps);
                     averageDPSoverTime = dpsList2.Average();
@@ -68,7 +92,7 @@ namespace DPSCalc
                         removeMaximum();
                     }
 
-                    if (dpsList2.Count() > 0)
+                    if (dpsList2.Count() > 0 || averageDPSoverTime != 0)
                     {
                         MelonLogger.Msg($"""
 
@@ -90,7 +114,7 @@ namespace DPSCalc
             }
             else
             {
-                return;
+                removeMaximum();
             }
         }
 
@@ -99,18 +123,17 @@ namespace DPSCalc
         {
             public static void Postfix(ref ITakeDamage _damageable, ref int _damageValue)
             {
-                if(_damageable is StatusEntity statusEntity && statusEntity._isPlayer == null)
+                if (_damageable is StatusEntity statusEntity && statusEntity._isPlayer == null)
                 {
                     MelonLogger.Msg($"[DMG] {_damageValue} hit.");
                     damageList.Add(_damageValue);
                     dpsList.Add(_damageValue);
-                    if(damageList.Count > 10)
+                    if (damageList.Count > 10)
                     {
                         damageList.RemoveAt(0);
                     }
                 }
             }
         }
-        
     }
 }
